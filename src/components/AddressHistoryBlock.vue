ff999a41820e7ac1c5493ca71ed0e31ea5e4d850
Temitope Oyeyinka <ttoyeyinka@gmail.com>
2024-08-12 18:32:17 +0100

    Final UI

--- src/components/AddressHistoryBlock.vue
+++ src/components/AddressHistoryBlock.vue
@@ -5,7 +5,12 @@ import axios from "axios";
 interface Address {
   addressLine1: string;
   postcode: string;
+  addressLine2: string;
+  city: string;
+  state: string;
   dateMovedIn: string;
+  dateMovedOut: string;
+  isStillLivingHere: boolean;
 }
 
 export default Vue.extend({
@@ -13,7 +18,15 @@ export default Vue.extend({
   data() {
     return {
       addresses: [
-        { addressLine1: "", postcode: "", dateMovedIn: "" },
+        {
+          addressLine1: "",
+          addressLine2: "",
+          city: "",
+          state: "",
+          postcode: "",
+          dateMovedIn: "",
+          isStillLivingHere: false,
+        },
       ] as Address[],
       errorMessage: "",
     };
@@ -39,8 +52,13 @@ export default Vue.extend({
       if (this.canAddMoreAddresses) {
         this.addresses.push({
           addressLine1: "",
+          addressLine2: "",
+          city: "",
+          state: "",
           postcode: "",
           dateMovedIn: "",
+          dateMovedOut: "",
+          isStillLivingHere: false,
         });
       }
     },
@@ -89,7 +107,12 @@ export default Vue.extend({
 
 <template>
   <div>
-    <div v-for="(address, index) in addresses" :key="index" class="mb-3">
+    <!-- Error Messages -->
+    <div v-if="errorMessage" class="text-danger my-3">
+      {{ errorMessage }}
+    </div>
+
+    <div v-for="(address, index) in addresses" :key="index" class="mt-5 mb-3">
       <div class="form-group">
         <label :for="'address-line1-' + index">Address Line 1</label>
         <input
@@ -100,47 +123,115 @@ export default Vue.extend({
           required
         />
       </div>
-      <div class="form-group">
-        <label :for="'postcode-' + index">Postcode</label>
+
+      <div class="form-group mt-3">
+        <label :for="'address-line2-' + index">Address Line 2 (optional)</label>
         <input
           type="text"
           class="form-control"
-          v-model="address.postcode"
-          :id="'postcode-' + index"
-          @input="fetchPostcodeSuggestions(index)"
-          required
+          v-model="address.addressLine2"
+          :id="'address-line2-' + index"
         />
-        <!-- Optional: Render postcode suggestions dropdown here -->
       </div>
-      <div class="form-group">
-        <label :for="'date-moved-in-' + index">Date Moved In</label>
-        <input
-          type="date"
-          class="form-control"
-          v-model="address.dateMovedIn"
-          :id="'date-moved-in-' + index"
-          required
-          @change="handleDateChange"
-        />
+
+      <div class="row g-3 mt-1">
+        <div class="col-sm-7">
+          <div class="form-group">
+            <label :for="'city-' + index">City</label>
+            <input
+              type="text"
+              class="form-control"
+              v-model="address.city"
+              :id="'city-' + index"
+              required
+            />
+          </div>
+        </div>
+
+        <div class="col-sm">
+          <div class="form-group">
+            <label :for="'state-' + index">State</label>
+            <input
+              type="text"
+              class="form-control"
+              v-model="address.state"
+              :id="'state-' + index"
+              required
+            />
+          </div>
+        </div>
+
+        <div class="col-sm">
+          <div class="form-group">
+            <label :for="'postcode-' + index">Postcode</label>
+            <input
+              type="text"
+              class="form-control"
+              v-model="address.postcode"
+              :id="'postcode-' + index"
+              @input="fetchPostcodeSuggestions(index)"
+              required
+            />
+            <!-- Optional: Render postcode suggestions dropdown here -->
+          </div>
+        </div>
+      </div>
+
+      <div class="row mt-3">
+        <div class="col">
+          <div class="form-group">
+            <label :for="'date-moved-in-' + index">Date Moved In</label>
+            <input
+              type="date"
+              class="form-control"
+              v-model="address.dateMovedIn"
+              :id="'date-moved-in-' + index"
+              required
+              @change="handleDateChange"
+            />
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="form-group">
+            <label :for="'date-moved-out-' + index">Date Moved Out</label>
+            <div class="input-group mb-3">
+              <div class="input-group-text">
+                <input
+                  class="form-check-input mt-0"
+                  type="checkbox"
+                  v-model="address.isStillLivingHere"
+                  value=""
+                  aria-label="Checkbox for following text input"
+                />
+              </div>
+              <input
+                type="date"
+                class="form-control"
+                v-model="address.dateMovedOut"
+                :disabled="address.isStillLivingHere"
+                :id="'date-moved-out-' + index"
+                required
+                @change="handleDateChange"
+              />
+            </div>
+            <div id="passwordHelpBlock" class="form-text text-start">
+              Check box if still living at this address
+            </div>
+          </div>
+        </div>
       </div>
     </div>
 
     <!-- Add Address Button -->
     <button
       v-if="canAddMoreAddresses"
-      class="btn btn-secondary"
+      class="btn btn-secondary my-3"
       @click="addAddress"
     >
       Add Another Address
     </button>
-
-    <!-- Error Messages -->
-    <div v-if="errorMessage" class="text-danger my-3">
-      {{ errorMessage }}
-    </div>
   </div>
 </template>
 
-<style scoped>
-/* Add custom styling here if necessary */
-</style>
+<style scoped></style>
--- src/views/FormView.vue
+++ src/views/FormView.vue
@@ -28,7 +28,10 @@ export default Vue.extend({
 
       if (addressHistoryBlock && addressHistoryBlock.validateBlock()) {
         // Proceed to create a claim or submit the form (createClaim.ts)
-        console.log("Form is valid and ready to be submitted.");
+        console.log(
+          "Form is valid and ready to be submitted.",
+          addressHistoryBlock
+        );
         // Your form submission logic goes here
       } else {
         console.log("Form validation failed.");
@@ -39,19 +42,19 @@ export default Vue.extend({
 </script>
 
 <template>
-  <div class="container">
+  <div class="container card py-3 px-5 shadow mb-5 w-100 w-lg-50 mx-lg-auto">
     <FormComponent>
       <template v-slot:title>
         <TextComponent
           :text="user ? `Welcome Back ${user.firstName}` : `Hello and welcome`"
-          type="h1"
+          type="h3"
           classes="mt-4 mb-2"
         />
       </template>
       <template v-slot:subtitle>
         <TextComponent
           text="Please provide your address history for the past 3 years"
-          type="h4"
+          type="h6"
           classes="mb-4"
         />
       </template>
@@ -64,3 +67,17 @@ export default Vue.extend({
     </FormComponent>
   </div>
 </template>
+
+<style scoped>
+@media (min-width: 992px) {
+  .w-lg-50 {
+    width: 50% !important;
+  }
+}
+
+@media (max-width: 768px) {
+  .w-lg-50 {
+    width: 65% !important;
+  }
+}
+</style>
